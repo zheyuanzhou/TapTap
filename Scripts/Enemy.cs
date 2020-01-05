816992e4b3b6438c2c433c7c06de7d38ca4a1dbe
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2020-01-04 21:13:08 -0500

    Add files via upload

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public Image hpImage;
+    public Image speedImage;
+
+    [SerializeField] private float maxHp;
+    [HideInInspector] public float hp;
+    [SerializeField] private float speed;
+    [HideInInspector] public float speedTimer;
+
+    public float attack;
+    public GameObject damageCanvas;
+
+    private void Start()
+    {
+        hp = maxHp;
+        speedTimer = speed;
+    }
+
+    private void Update()
+    {
+        SpeedRate();
+        HealthBar();
+    }
+
+    public void Hurt(float _enemyAmount)
+    {
+        damageCanvas.GetComponentInChildren<Text>().text = _enemyAmount.ToString();
+        Instantiate(damageCanvas, transform.position, Quaternion.identity);
+
+        hp -= _enemyAmount;
+
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void HealthBar()
+    {
+        hpImage.fillAmount = hp / maxHp;
+    }
+
+    private void SpeedRate()
+    {
+        speedTimer -= Time.deltaTime;
+        if (speedTimer <= 0)
+        {
+            GameManager.instance.EnemyAttack(attack);
+            speedTimer = speed;
+        }
+
+        speedImage.fillAmount = speedTimer / speed;
+    }
+
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public List<Character> characters = new List<Character>();
+    public List<Enemy> enemies = new List<Enemy>();
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void EnemyAttack(float _enemyAttack)
+    {
+        foreach(Character character in characters)
+        {
+            if(character != null)
+            {
+                character.Hurt(_enemyAttack);
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
+
+    public void CharacterAttack(float _characterAttack)
+    {
+        foreach(Enemy enemy in enemies)
+        {
+            if(enemy != null)
+            {
+                enemy.Hurt(_characterAttack);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+}
